b85a08249093634aee7cea58852a9c42408071ef
kiril <karkarkarkirill@mail.ru>
2021-02-21 14:57:35 +0700

    Добавлен проект логики

--- NoteAppUI/NoteAppUI/MainForm23.Designer.cs
+++ NoteAppUI/NoteAppUI/MainForm23.Designer.cs
@@ -29,10 +29,18 @@ namespace NoteAppUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm23
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainForm23";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm23_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
