18941e4e25cf46974e46f5d68ac1807c42600096
kiril <karkarkarkirill@mail.ru>
2021-02-23 13:45:52 +0700

    Создан класс Заметки и метод Описание

--- NoteAppUI/NoteAppUI/MainForm23.cs
+++ NoteAppUI/NoteAppUI/MainForm23.cs
@@ -12,16 +12,41 @@ using NoteApp;
 
 namespace NoteAppUI
 {
+
     public partial class MainForm23 : Form
     {
         public MainForm23()
         {
+
             InitializeComponent();
+
         }
 
         private void MainForm23_Load(object sender, EventArgs e)
         {
 
         }
+
+        public void Description()
+        {
+
+            {
+                var note = new Note();
+                note.Name = new string[]
+                    {"Название"};
+                note.Text = new string[]
+                {
+                    "Напомнить завтра Юле в 7 утра"
+                };
+                note.Time = 11;
+            }
+        }
+
+
+
     }
 }
+        
+
+    
+
