a44c6d5ca89b53e62aa01e5c37ff283a4a8b3bed
kiril <karkarkarkirill@mail.ru>
2021-02-24 17:37:44 +0700

    Созданы свойства для текста, названия и категорий заметок

--- NoteAppUI/NoteApp/Note.cs
+++ NoteAppUI/NoteApp/Note.cs
@@ -12,17 +12,68 @@ namespace NoteApp
     public class Note
     {
         /// <summary>
+        /// Имя заметки.
+        /// </summary>
+        private string _name;
+        /// <summary>
+        /// Текст заметки.
+        /// </summary>
+        private string _text;
+        /// <summary>
+        /// Категории заметки.
+        /// </summary>
+        private string _categoryNotes;
+
+        /// <summary>
         /// Возвращает и задает имя заметки.
         /// </summary>
-        public string[] Name { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException("Название должно содержать не больше 50 символов ");
+                }
+                else _name = value;
+
+            }
+        }
         /// <summary>
         /// Возвращает и задает текст пользователя.
         /// </summary>
-        public string[] Text { get; set; }
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (value.Length > 1500)
+                {
+                    throw new ArgumentException("Текст ,временно, не может содержать более 1500 символов");
+                }
+                else _name = value;
+
+            }
+        }
         /// <summary>
         /// Возвращает и задает категории пользователя.
         /// </summary>
-        public string[] CategoryNotes { get; set; }
+        public string CategoryNotes
+        {
+            get { return _categoryNotes; }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException("Категория не должна содержать больше 50 символов ");
+                }
+                else _name = value;
+
+            }
+        }
+        
+
         /// <summary>
         /// Задает время создания заметки
         /// </summary>
