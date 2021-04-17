a577164a9471d5247a4759b5f113f0974c21e4e3
kiril <karkarkarkirill@mail.ru>
2021-04-17 14:02:39 +0700

    Исправление ошибок репозитория

--- NoteAppUI/NoteAppUI/MainForm23.Designer.cs
+++ NoteAppUI/NoteAppUI/MainForm23.Designer.cs
@@ -1,1 +1,241 @@
 ﻿
+namespace NoteAppUI
+{
+    partial class MainForm23
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm23));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(65, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(132, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Help";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button3_KeyDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 65);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(-1, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 30);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Show Category";
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(12, 396);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(47, 42);
+            this.button5.TabIndex = 11;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // button6
+            // 
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(65, 396);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(47, 42);
+            this.button6.TabIndex = 12;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(118, 396);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(45, 42);
+            this.button7.TabIndex = 13;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 260);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.Click += new System.EventHandler(this.Click_listBox1);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(273, 78);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(515, 341);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Location = new System.Drawing.Point(286, 40);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 18;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Enabled = false;
+            this.dateTimePicker2.Location = new System.Drawing.Point(588, 40);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(585, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Modified";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Created";
+            // 
+            // MainForm23
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm23";
+            this.Text = "NoteApp";
+            this.Load += new System.EventHandler(this.MainForm23_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
--- NoteAppUI/NoteAppUI/MainForm23.cs
+++ NoteAppUI/NoteAppUI/MainForm23.cs
@@ -1,1 +1,257 @@
-﻿
+﻿using System;
+using System.Windows.Forms;
+using NoteApp;
+
+
+namespace NoteAppUI
+{
+
+    public partial class MainForm23 : Form
+    {
+        private Project _project;
+        private Note _note = new Note();
+        private Form1 F1 = new Form1();
+        public MainForm23()
+        {
+            InitializeComponent();
+
+            comboBox1.DataSource = Enum.GetValues(typeof(NoteCategory));
+            _project = ProjectManager.LoadFromFile(ProjectManager.FileName);
+           
+            UpdateNotesListBox();
+            listBox1.DisplayMember = "name";
+            
+        }
+
+        /// <summary>
+        /// Обновление списка заметок.
+        /// </summary>
+        private void UpdateNotesListBox()
+        {
+            listBox1.Items.Clear();
+            if (_project != null)
+            {
+                for (int i = 0; i < _project.Notes.Count; i++)
+                {
+                    if (_project.Notes[i].Name != "")
+                        listBox1.Items.Add(_project.Notes[i].Name);
+                    else
+                        listBox1.Items.Add("Без названия");
+                }
+
+            }
+        }
+
+        private void AddNote()
+        {
+            var addForm = new Form1();
+            addForm.ShowDialog();
+
+            if (addForm.DialogResult == DialogResult.OK)
+            {
+                var addedNote = addForm.NoteData;
+                _project.Notes.Add(addedNote);
+                listBox1.Items.Add(addedNote);
+                UpdateNotesListBox();
+            }
+            else return;
+
+            ProjectManager.SaveToFile(_project, ProjectManager.FileName);
+        }
+
+        private void EditNote()
+        {
+            var selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("Не выбрана запись для редактирования", "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                var selectedNote = _project.Notes[selectedIndex];
+
+                var editForm = new Form1();
+                editForm.NoteData = selectedNote;
+                editForm.ShowDialog();
+
+                if (editForm.DialogResult == DialogResult.OK)
+                {
+                    var editedNote = editForm.NoteData;
+
+                    _project.Notes.Insert(selectedIndex, editedNote);
+                    listBox1.Items.Insert(selectedIndex, editedNote.Name);
+                    _project.Notes.RemoveAt(selectedIndex + 1);
+                    UpdateNotesListBox();
+                    listBox1.SetSelected(selectedIndex, true);
+                    ProjectManager.SaveToFile(_project, ProjectManager.FileName);
+                }
+                else return;
+            }
+            catch
+            {
+                MessageBox.Show("Запись не найдена", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public MainForm23(string text) // <-- Новый конструктор формы
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void MainForm23_Load(object sender, EventArgs e)
+        
+        {
+            ToolTip createTip = new ToolTip(); 
+            createTip.SetToolTip(button5, "Создать заметку");
+
+            ToolTip changeTip = new ToolTip(); 
+            changeTip.SetToolTip(button6, "Изменить заметку");
+
+            ToolTip deleteTip = new ToolTip();
+            deleteTip.SetToolTip(button7, "Удалить заметку");
+        }
+        private void RemoveNote()
+        {
+            var selectedIdex = listBox1.SelectedIndex;
+            if (selectedIdex == -1)
+            {
+                MessageBox.Show("Не выбрана запись для удаления", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var dialogResult = MessageBox.Show("Вы дейcтвительно хотите удалить запись?", "Удаление записи", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.OK)
+            {
+                _project.Notes.RemoveAt(selectedIdex);
+                UpdateNotesListBox();
+            }
+
+          
+            ProjectManager.SaveToFile(_project, ProjectManager.FileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            About frm = new About();
+            frm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // получение заметки
+            var selectedNote = (Note)listBox1.SelectedItem;
+
+
+            dateTimePicker1.Value = selectedNote.TimeCreated;
+            dateTimePicker2.Value = selectedNote.TimeModified;
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            AddNote();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            EditNote();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            RemoveNote();
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+        //вывод значения
+        private void Click_listBox1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                textBox3.Text = _project.Notes[listBox1.SelectedIndex].Text;
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+        
+
+    
+
