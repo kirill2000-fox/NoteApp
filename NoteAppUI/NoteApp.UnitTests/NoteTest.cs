36f598f0d07f879c80f58d96c8a43aa9aa8cc269
kiril <karkarkarkirill@mail.ru>
2021-05-02 20:53:23 +0700

    Создан юнит тест Name, работает неисправно.

--- NoteAppUI/NoteApp.UnitTests/NoteTest.cs
+++ NoteAppUI/NoteApp.UnitTests/NoteTest.cs
@@ -19,16 +19,70 @@ namespace NoteApp.UnitTests
             _note.Name = "Имя заметки один два три";
             _note.Category = NoteCategory.Job;
             _note.Text = "Текст заметки три два один";
-            //_note.timeCreated = DateTime.Now;
-            //_note._timeModified = DateTime.Now;
-        }
-        [Test]
-        public void Name_GoodName_ReturnsSameName()
-        {
-            var note = new Note(NoteCategory.Job, "Имя заметки один два три", "Текст заметки три два один");
-            note.Name = sourceName;
-            _note.Name = sourceName;
-        }
+            
 
         }
+
+        /// <summary>
+        /// Проверка на ввод правильного названия заметки
+        /// </summary>
+        [Test(Description = "Позитивный тест геттера Name")]
+        public void Name_GoodName_ReturnsSameName()
+        {
+            //Ожидаемое название заметки
+            var expected = _note.Name;
+
+            //Присваиваем полю "Название заметки" текст
+            _note.Name = expected;
+
+            //Присваиваем полю, хранящему название
+            //заметки в реальном времени новое значение
+            var actual = _note.Name;
+
+            //Сравниваем ожидаемое значение с тем, что получили
+            Assert.AreEqual(expected, actual,
+                "Геттер Title возвращает неправильное название заметки");
+        }
+
+        /// <summary>
+        /// Проверка на ввод пустой строки в название заметки
+        /// </summary>
+        [Test(Description = "Тест геттера Name с пустым значением")]
+        public void TestNameGet_EmptyValue()
+        {
+            //Ожидаемое название заметки
+            var expected = "Без названия";
+
+            //Присваиваем пустое значение name
+            _note.Name = "";
+
+            //Присваиваем значение из поля "Название заметки"
+            var actual = _note.Name;
+
+            //Сравниваем ожидаемое значение с полученным
+            Assert.AreEqual(expected, actual,
+                "Геттер Name возвращает неверное название заметки при пустом значении");
+        }
+
+        /// <summary>
+        /// Проверка на ввод названия заметки более 50 символов
+        /// </summary>
+        [Test(Description = "Тест геттера Name с длиной значения" +
+                            "поля, превышающего 50 символов")]
+        public void TestNameGet_TooLongValue()
+        {
+            var source = "Слишком длинное название поля," +
+                         "превышающее 50 символов и выбрасывающее" +
+                         "исключение";
+
+            Assert.Throws<ArgumentException>(
+                //Анонимный метод
+                () =>
+                {
+                    //Тестируемый код
+                    _note.Name = source;
+                }, "Должно возникать исключение, если длина названия" +
+                   "более 50 символов");
+        }
+    }
 }
