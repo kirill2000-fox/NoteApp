35833f7f17004f16ca3dc990c4776b85c94e3c92
kiril <karkarkarkirill@mail.ru>
2021-05-17 12:39:34 +0700

    Написан ProjectTests

---
+++ NoteAppUI/NoteApp.UnitTests/ProjectTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NoteApp.UnitTests
+{
+    [TestFixture]
+    class ProjectTest
+    {
+        private Project _project;
+
+        [Test(Description = "Позитивный тест геттера Notes")]
+        public void Notes_CorrectValue_ReturnsSameValue()
+        {
+            // Setup
+            _project = new Project();
+
+            var expected = new List<Note>
+            {
+                new Note(NoteCategory.Documents),
+                new Note(NoteCategory.Home),
+                new Note(NoteCategory.HealthAndSport)
+            };
+
+            _project.Notes = expected;
+
+            // Act
+            var actual = _project.Notes;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер Notes возвращает неправильный список заметок");
+        }
+
+        [Test(Description = "Позитивный тест сеттера Notes")]
+        public void Notes_CorrectValue_SetsSameValue()
+        {
+            // Setup
+            _project = new Project();
+
+            var expected = new List<Note>
+            {
+                new Note(NoteCategory.Documents),
+                new Note(NoteCategory.Home),
+                new Note(NoteCategory.HealthAndSport)
+            };
+
+            // Act
+            _project.Notes = expected;
+            var actual = _project.Notes;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер Notes присваивает неправильный список заметок");
+        }
+    }
+}
