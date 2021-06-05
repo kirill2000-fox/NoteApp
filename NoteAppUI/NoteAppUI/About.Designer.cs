e43b72241e4cb6890fdc36e8f41f443889cbde82
kiril <karkarkarkirill@mail.ru>
2021-06-05 11:56:22 +0700

    Исправление About

--- NoteAppUI/NoteAppUI/About.Designer.cs
+++ NoteAppUI/NoteAppUI/About.Designer.cs
@@ -43,7 +43,8 @@ namespace NoteAppUI
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(38, 37);
+            this.label1.Location = new System.Drawing.Point(7, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 39);
             this.label1.TabIndex = 0;
@@ -53,7 +54,7 @@ namespace NoteAppUI
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 95);
+            this.label2.Location = new System.Drawing.Point(16, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 17);
             this.label2.TabIndex = 1;
@@ -62,7 +63,7 @@ namespace NoteAppUI
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 138);
+            this.label3.Location = new System.Drawing.Point(16, 103);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(143, 17);
             this.label3.TabIndex = 2;
@@ -71,7 +72,7 @@ namespace NoteAppUI
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(45, 172);
+            this.label4.Location = new System.Drawing.Point(16, 153);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(137, 17);
             this.label4.TabIndex = 3;
@@ -80,7 +81,7 @@ namespace NoteAppUI
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(176, 172);
+            this.linkLabel1.Location = new System.Drawing.Point(147, 153);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(148, 17);
             this.linkLabel1.TabIndex = 4;
@@ -91,7 +92,7 @@ namespace NoteAppUI
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(45, 204);
+            this.label5.Location = new System.Drawing.Point(16, 185);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(60, 17);
             this.label5.TabIndex = 5;
@@ -100,7 +101,7 @@ namespace NoteAppUI
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(97, 204);
+            this.linkLabel2.Location = new System.Drawing.Point(68, 185);
             this.linkLabel2.Name = "linkLabel2";
             this.linkLabel2.Size = new System.Drawing.Size(262, 17);
             this.linkLabel2.TabIndex = 6;
@@ -111,7 +112,7 @@ namespace NoteAppUI
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(45, 421);
+            this.label6.Location = new System.Drawing.Point(16, 422);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(143, 17);
             this.label6.TabIndex = 7;
